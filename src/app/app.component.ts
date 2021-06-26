5cf370286a675981e4cc62ed340f6b23a98e5c46
Tirth <tirthcoder@gmail.com>
2021-06-26 20:52:43 +0530

    Password Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwg';
+  password = "";
+  length = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  onChangeLength(value: string){
+    this.length = parseInt(value);
+  }
+
+  onChangeUseLetters(){
+    this.useLetters = !this.useLetters;  
+  }
+
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onGenerate(){
+    let numbers = "1234567890";
+    let letters = "abcdefghijkmnopqrstuvwxyz";
+    let symbols = "@#$!()&<>?";
+
+    let validChars = "";
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+
+    if(this.useLetters){
+      validChars += letters;
+    }
+
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = "";
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
